9c148658238a4264572b4fc1adf69893dd50034a
buchuitoudegou <756541536@qq.com>
2018-12-10 13:35:55 +0800

    initial

---
+++ app/src/main/java/com/example/a75654/salad_cream/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a75654.salad_cream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
