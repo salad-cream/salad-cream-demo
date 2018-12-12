5450f514f0ed6a62306421ed3fa827f09e515705
liuyh73 <15989067460@163.com>
2018-12-12 17:42:54 +0800

    社区界面

---
+++ app/src/main/java/com/example/a75654/salad_cream/community/Recommend.java
@@ -0,0 +1,17 @@
+package com.example.a75654.salad_cream.community;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.a75654.salad_cream.R;
+
+public class Recommend extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.community_recommend,null);
+        return view;
+    }
+}
