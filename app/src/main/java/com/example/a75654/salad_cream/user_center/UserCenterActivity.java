6c3a01d3a3ec77c0fdfcd88f2f5c7ace8995ebd6
liuwd8 <2474223242@qq.com>
2018-12-12 15:46:53 +0800

    user center UI

---
+++ app/src/main/java/com/example/a75654/salad_cream/user_center/UserCenterActivity.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @description 用户中心界面
+ * @author liuwd8
+ * @created 2018/12/12
+ * @updated updated date
+ *
+ */
+
+package com.example.a75654.salad_cream.user_center;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class UserCenterActivity extends AppCompatActivity {
+}
