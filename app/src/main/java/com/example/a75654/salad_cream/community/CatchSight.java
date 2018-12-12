0137bc4bc27d4c6cf2c94439af86cc5d575228fd
liuyh73 <15989067460@163.com>
2018-12-12 21:47:51 +0800

    更改图片加载方式

--- app/src/main/java/com/example/a75654/salad_cream/community/CatchSight.java
+++ app/src/main/java/com/example/a75654/salad_cream/community/CatchSight.java
@@ -5,13 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.a75654.salad_cream.R;
 
 public class CatchSight extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.community_catch_sight,null);
+
+        Glide.with(CatchSight.this).load(R.mipmap.catchsight1).into((ImageView) view.findViewById(R.id.catchsight1));
+        Glide.with(CatchSight.this).load(R.mipmap.catchsight2).into((ImageView) view.findViewById(R.id.catchsight2));
+
         return view;
     }
 }
